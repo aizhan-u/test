dd3edb7a7339fba48289cce1ba02f4c6724aa67f
Aizhan <aizhan.uristembek@nu.edu.kz>
2020-12-21 11:30:14 +0600

    basic setup

--- src/App.vue
+++ src/App.vue
@@ -6,34 +6,15 @@
       dark
     >
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        {{name}}
       </div>
 
-      <v-spacer></v-spacer>
+      <v-spacer/>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <v-icon>mdi-cart-variant</v-icon>
       </v-btn>
     </v-app-bar>
 
@@ -44,6 +25,7 @@
 </template>
 
 <script>
+import { get } from './helpers/api'
 import Dialog from './components/Dialog';
 
 export default {
@@ -54,7 +36,19 @@ export default {
   },
 
   data: () => ({
-    //
+    name: ''
   }),
+
+  methods: {
+    getInfo() {
+      get(this, '', '', response=>{
+        this.name = response.data.name;
+      })
+    },
+  },
+
+  created() {
+    this.getInfo();
+  },
 };
 </script>
--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -1,14 +1,69 @@
 <template>
-  <v-dialog>
-    
+  <v-dialog
+    v-model="popup">
+    <v-card>
+      <v-card-title>
+        {{info.name}}
+        <v-spacer />
+        <v-icon
+          aria-label="Close"
+          @click="popup=false"
+        >
+          mdi-close
+        </v-icon>
+      </v-card-title>
+
+      <v-card-text>
+        <v-checkbox
+          v-model="status.availableProducts">
+        </v-checkbox>
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          @click="popup = false"
+        >
+          Close
+        </v-btn>
+      </v-card-actions>
+    </v-card>
   </v-dialog>
 </template>
 
 <script>
+  import { get } from '../helpers/api'
   export default {
     name: 'Dialog',
 
     data: () => ({
+      popup: true,
+      info: '',
+      status: '',
+      cities: []
     }),
+
+    computed: {
+    },
+
+    watch: {
+    },
+
+    methods: {
+      getInfo() {
+        get(this, '', '', response=>{
+          this.info = response.data;
+          this.status = this.info.status;
+          this.cities = this.info.cities;
+          console.log(this.info);
+        })
+      },
+    },
+
+    created() {
+      this.getInfo();
+    },
   }
 </script>
